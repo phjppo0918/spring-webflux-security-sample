f2b3113263c07e1957ac8eff6c534cc29e04094d
Hyeonjun Park <phjppo0918@gmail.com>
2023-05-25 17:21:33 +0900

    init setting

---
+++ src/main/java/com/example/springwebfluxsecuritysample/SpringWebfluxSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxsecuritysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxSecuritySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxSecuritySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springwebfluxsecuritysample/SpringWebfluxSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxsecuritysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxSecuritySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
